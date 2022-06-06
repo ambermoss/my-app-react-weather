4c91e90ec7645bacbe910093822ab3b80f014cda
amber moss <amoss@mossmedia.net>
2022-06-06 11:38:27 -0500

    moved code sandbox over to vs code

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-        <Weather city="Paris" />
-      </header>
+      <Weather />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,73 @@
 import React from "react";
-import axios from "axios";
+import "./Weather.css";
 
 export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
-    );
-  }
-  let apiKey = "30d8dcf8c5a32b629f0453f6b9714950";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&unites=metric`;
-  axios.get(apiUrl).then(handleResponse);
-  return <h2>Hello from Weather App</h2>;
+  let weatherData = {
+    city: "St. Louis",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+    date: "Friday, 11:00",
+    humidity: 55,
+    wind: 10,
+    lowTemp: 53,
+    currentTemp: 68,
+  };
+  return (
+    <div className="Weather">
+      <div className="my-weather-app-wrapper">
+        <h1>Weather</h1>
+        <div className="container">
+          <form>
+            <div className="row">
+              <div className="col-5">
+                <div className="mb-3">
+                  <input
+                    type="search"
+                    placeholder="Enter the city name"
+                    className="form-control"
+                    aria-describedby="emailHelp"
+                    autofocus
+                    autocomplete="off"
+                  />
+                </div>
+              </div>
+              <div className="col-3">
+                <button type="submit" className="btn btn-primary w-100">
+                  Search
+                </button>
+              </div>
+              <div className="col-3">
+                <button className="btn btn-success w-100">Current City</button>
+              </div>
+            </div>
+          </form>
+
+          <div className="row">
+            <div className="col-5">
+              <h2>{weatherData.city}</h2>
+              <div className="today-details-header">
+                <span className="current-temp">{weatherData.currentTemp}</span>
+                <span className="units"> °F</span>
+              </div>
+            </div>
+
+            <div className="col-6">
+              <ul className="current-conditions">
+                <img
+                  src={weatherData.imgUrl}
+                  alt="weather condition"
+                  className="float-left"
+                />
+
+                <li>{weatherData.date}</li>
+                <li>Humidity: {weatherData.humidity}%</li>
+                <li>Wind: {weatherData.wind}Km/H</li>
+                <li></li>
+                <li>L: {weatherData.lowTemp}°</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
